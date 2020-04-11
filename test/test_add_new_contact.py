fd13f5fe6e02efb1f1072eca0de1e9d7752c6c8b
havrylyshyn <bohdan.havrylyshyn@infopulse.com>
2020-04-11 23:30:25 +0300

    session control added to fixture

--- test/test_add_new_contact.py
+++ test/test_add_new_contact.py
@@ -3,12 +3,8 @@ from model.contact import Contact
 
 
 def test_add_new_contact(app):
-    app.session.login(username="admin", password="secret")
     app.contact.create(Contact(firstName="Bohdan", lastName="Havrylyshyn", address="UA, Kyiv, KPI", phoneHome="0123456789", email="test@mail.com"))
-    app.session.logout()
 
 
 def test_add_empty_contact(app):
-    app.session.login(username="admin", password="secret")
     app.contact.create(Contact(firstName="", lastName="", address="", phoneHome="", email=""))
-    app.session.logout()
