68d8873025e905f490ae691bfa78d8fb42dd5a62
havrylyshyn <bohdan.havrylyshyn@infopulse.com>
2020-05-06 17:34:27 +0300

    configuration file added

--- fixture/contact.py
+++ fixture/contact.py
@@ -52,7 +52,12 @@ class ContactHelper:
         self.change_field_value("lastname", contact.lastname)
         self.change_field_value("address", contact.address)
         self.change_field_value("home", contact.homephone)
+        self.change_field_value("mobile", contact.mobilephone)
+        self.change_field_value("work", contact.workphone)
+        self.change_field_value("phone2", contact.secondaryphone)
         self.change_field_value("email", contact.email)
+        self.change_field_value("email2", contact.email2)
+        self.change_field_value("email3", contact.email3)
 
     def change_field_value(self, field_name, text):
         wd = self.app.wd
