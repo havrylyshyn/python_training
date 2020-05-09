45f81d939eb87dc73c928515f6fdeef8b6533201
havrylyshyn <bohdan.havrylyshyn@infopulse.com>
2020-05-09 21:16:08 +0300

    static data added dynamic data generator added

--- generator/contact.py
+++ generator/contact.py
@@ -2,7 +2,7 @@ from model.contact import Contact
 import random
 import string
 import os.path
-import json
+import jsonpickle
 import getopt
 import sys
 
@@ -14,7 +14,7 @@ except getopt.GetoptError as err:
     sys.exit(2)
 
 n = 5
-f = "data/contact.json"
+f = "data/contacts.json"
 
 for o, a in opts:
     if o == "-n":
@@ -52,4 +52,5 @@ test_data = [Contact(firstname="", lastname="", address="", homephone="", email=
 file = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", f)
 
 with open(file, "w") as out:
-    out.write(json.dumps(test_data, default=lambda x: x.__dict__, indent=2))
+    jsonpickle.set_encoder_options("json", indent=2)
+    out.write(jsonpickle.encode(test_data))
